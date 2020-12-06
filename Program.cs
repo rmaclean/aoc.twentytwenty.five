5ae79574917ff78919de62094b996bb55f1cf708
Robert MacLean <robert@sadev.co.za>
2020-12-06 10:11:07 +0200

    end of part two

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@ using System.IO;
 using System.Linq;
 
 var fileData = await File.ReadAllLinesAsync("data.txt");
-var seatData = fileData.AsParallel().Select(raw => new Seat(raw)).ToArray();
+var seatData = fileData.AsParallel().Select(raw => new Seat(raw)).OrderBy(_ => _.ID).ToArray();
 
-var highestSeat = seatData.Max(seat => seat.ID);
-Console.WriteLine($"💺 {highestSeat}");
+var expectedID = seatData[0].ID + 1;
+for (var index = 1; index < seatData.Length -1; index++, expectedID++)
+{
+   if (seatData[index].ID != expectedID) {
+       Console.WriteLine($"Empty seat at {expectedID}");
+       break;
+   }
+}
